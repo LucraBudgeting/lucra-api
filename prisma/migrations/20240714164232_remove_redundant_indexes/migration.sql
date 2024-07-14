914839cefbf3340bf21054aef026c5a6ace23cf2
Ryan H <ryanzhutch@gmail.com>
2024-07-14 10:43:09 -0600

    refactor: Remove redundant indexes from database schema

---
+++ prisma/migrations/20240714164232_remove_redundant_indexes/migration.sql
@@ -0,0 +1,20 @@
+-- DropIndex
+DROP INDEX "transaction_category_user_id_index";
+
+-- DropIndex
+DROP INDEX "user_email_index";
+
+-- DropIndex
+DROP INDEX "user_username_index";
+
+-- DropIndex
+DROP INDEX "user_auth_user_id_index";
+
+-- DropIndex
+DROP INDEX "user_onboarding_step_user_id_index";
+
+-- DropIndex
+DROP INDEX "user_preferences_user_id_index";
+
+-- DropIndex
+DROP INDEX "user_session_user_id_index";
