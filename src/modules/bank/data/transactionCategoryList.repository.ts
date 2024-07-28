2273c43b82945ac2b8c4011751422b2ee2910f1b
Ryan H <ryanzhutch@gmail.com>
2024-07-28 16:29:55 -0600

    refactor: Update transaction category list repository to format primary values

--- src/modules/bank/data/transactionCategoryList.repository.ts
+++ src/modules/bank/data/transactionCategoryList.repository.ts
@@ -10,6 +10,18 @@ const csvFilePath = path.resolve(
   'documents/transactions-personal-finance-category-taxonomy.csv'
 );
 
+function formatPrimary(val: any): string {
+  if (!val) {
+    return '';
+  }
+
+  let str = val.toString() as string;
+
+  str = str.replace(/_/g, ' ');
+
+  return str.toLowerCase().replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase());
+}
+
 async function readCsvToRecord(filePath: string): Promise<CsvRecord> {
   return await new Promise((resolve, reject) => {
     const record: CsvRecord = {};
@@ -18,7 +30,7 @@ async function readCsvToRecord(filePath: string): Promise<CsvRecord> {
       .pipe(csv())
       .on('data', (row) => {
         const key = row.DETAILED;
-        const value = row.DESCRIPTION;
+        const value = `${formatPrimary(row.PRIMARY)} - ${row.DESCRIPTION}`;
         if (key && value) {
           record[key] = value;
         }
@@ -34,8 +46,6 @@ async function readCsvToRecord(filePath: string): Promise<CsvRecord> {
 
 export async function getCategoryListDictionary(): Promise<CsvRecord> {
   try {
-    console.log('Current directory:', process.cwd());
-
     return await readCsvToRecord(csvFilePath);
   } catch (error) {
     logger.error('Error reading CSV file:', error);
