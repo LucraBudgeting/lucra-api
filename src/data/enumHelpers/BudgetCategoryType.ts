2733aeb76028699e7c067b4ca7567d86a765005b
Ryan H <ryanzhutch@gmail.com>
2024-08-02 12:14:57 -0600

    refactor: Update budget category type mapping functions to include 'transfer'

--- src/data/enumHelpers/BudgetCategoryType.ts
+++ src/data/enumHelpers/BudgetCategoryType.ts
@@ -1,19 +1,24 @@
 import { BudgetCategoryType } from '@prisma/client';
+import { budgetCategoryType } from '@/modules/budget/types/category';
 
-export function mapStringToBudgetType(budgetType: 'debit' | 'credit'): BudgetCategoryType {
+export function mapStringToBudgetType(budgetType: budgetCategoryType): BudgetCategoryType {
   switch (budgetType) {
     case 'debit':
       return BudgetCategoryType.Debit;
     case 'credit':
       return BudgetCategoryType.Credit;
+    case 'transfer':
+      return BudgetCategoryType.Transfer;
   }
 }
 
-export function mapBudgetTypeToString(budgetType: BudgetCategoryType): 'debit' | 'credit' {
+export function mapBudgetTypeToString(budgetType: BudgetCategoryType): budgetCategoryType {
   switch (budgetType) {
     case BudgetCategoryType.Debit:
       return 'debit';
     case BudgetCategoryType.Credit:
       return 'credit';
+    case BudgetCategoryType.Transfer:
+      return 'transfer';
   }
 }
