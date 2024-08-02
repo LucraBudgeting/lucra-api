d04c7037f69d3c7a06ae2b3aa0641b0f1d85c5c9
Ryan H <ryanzhutch@gmail.com>
2024-08-02 16:13:02 -0600

    refactor: Add logging for syncing transactions with Plaid API

--- src/libs/pgBoss/pgBossJobController.ts
+++ src/libs/pgBoss/pgBossJobController.ts
@@ -1,6 +1,7 @@
 import { accountRepository } from '@/data/repositories/account.repository';
 import { plaidRepository } from '../plaid/plaid.repository';
 import { boss } from './pgBossConfig';
+import { logger } from '../logger';
 
 boss.work(
   'sync-transaction-history',
@@ -8,6 +9,9 @@ boss.work(
     const { userId, accountIds, itemId } = job.data;
     const { cursor, accessToken } =
       await accountRepository.getLatestCursorFromAccountItemId(itemId);
+    if (cursor) {
+      logger.warn('Syncing Transactions', { cursor, itemId });
+    }
     await plaidRepository.syncTransactionHistory(userId, accountIds, accessToken, cursor);
   }
 );
