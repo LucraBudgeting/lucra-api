26e5a8822b979a638f73fd6a4c9b5589c304dfcd
Ryan H <ryanzhutch@gmail.com>
2024-08-10 11:36:57 -0600

    feat: Add billing routes and controller for handling billing portal URL

--- src/data/repositories/userBilling.repository.ts
+++ src/data/repositories/userBilling.repository.ts
@@ -1,4 +1,5 @@
 import { BillingPlatform } from '@prisma/client';
+import { NotFoundError } from '@/exceptions/error';
 import { BaseRepository } from './base.repository';
 
 class UserBillingRepository extends BaseRepository {
@@ -20,6 +21,37 @@ class UserBillingRepository extends BaseRepository {
         userId,
       },
     });
+  }
+
+  public async getUserBillingCustomerId(userId: string): Promise<string> {
+    const userBilling = await this.getUserBillingByUserId(userId);
+
+    if (!userBilling) {
+      throw new NotFoundError('User billing not found');
+    }
+
+    switch (userBilling.billingPlatform) {
+      case BillingPlatform.STRIPE:
+        if (userBilling.stripeCustomerId) {
+          return userBilling.stripeCustomerId;
+        } else {
+          throw new NotFoundError('Stripe customer id not found');
+        }
+      case BillingPlatform.APPLE:
+        if (userBilling.appleReceipt) {
+          return userBilling.appleReceipt;
+        } else {
+          throw new NotFoundError('Stripe customer id not found');
+        }
+      case BillingPlatform.GOOGLE:
+        if (userBilling.googleOrderId) {
+          return userBilling.googleOrderId;
+        } else {
+          throw new NotFoundError('Stripe customer id not found');
+        }
+      default:
+        throw new Error('Unsupported billing platform');
+    }
   }
 }
 
